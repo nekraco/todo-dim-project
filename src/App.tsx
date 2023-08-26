d94f85e54d4cb6722b259c1e695dce6f30fff926
nekraco <UrtaevAndreyA@gmail.com>
2023-08-26 20:32:41 +0300

    Created data in the form of arrays of objects for lists

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,25 @@ import {Todolist} from "./Todolist";
 
 export function App() {
 
+    let tasks1 = [
+        {id: 1, title: 'CSS', isDone: true},
+        {id: 2, title: 'JS', isDone: false},
+        {id: 3, title: 'React', isDone: true},
+    ];
+
+    let tasks2 = [
+        {id: 1, title: 'Terminator', isDone: true},
+        {id: 2, title: 'XXX', isDone: false},
+        {id: 3, title: 'Gentlemen of fortune', isDone: true},
+    ];
+
+
+
     return (
         <div className="App">
             <Todolist title={"What to learn"}/>
             <Todolist title={"Movies"}/>
-            <Todolist title={"Songs"}/>
+
             
         </div>
     );
